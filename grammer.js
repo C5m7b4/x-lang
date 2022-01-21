46b6be4d78abc9b283713a959d6376ab183d2f66
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-21 13:34:25 -0600

    add sequence literal

--- grammer.js
+++ grammer.js
@@ -94,25 +94,41 @@ var grammar = {
     {"name": "expression", "symbols": ["literal"], "postprocess": id},
     {"name": "expression", "symbols": ["function_call"], "postprocess": id},
     {"name": "expression", "symbols": ["code_block"], "postprocess": id},
-    {"name": "expression", "symbols": ["array_literal"], "postprocess": id},
     {"name": "literal", "symbols": [(lexer.has("number") ? {type: "number"} : number)], "postprocess": id},
     {"name": "literal", "symbols": [(lexer.has("string") ? {type: "string"} : string)], "postprocess": id},
-    {"name": "array_literal", "symbols": [{"literal":"{"}, "_", "expression_list", "_", {"literal":"}"}], "postprocess": 
+    {"name": "literal", "symbols": ["sequence_literal"], "postprocess": id},
+    {"name": "sequence_literal", "symbols": ["optional_tag", {"literal":"{"}, "_", "expression_list", "_", {"literal":"}"}], "postprocess": 
         (data) => {
+          const tagName = data[0] || 'array';
+          if ( tagName === 'dict'){
+            throw new error('You tagged a sequence as a dict')
+          }
           return {
-            type: 'array_literal',
-            items: data[2]
+            type: tagName + '_literal',
+            items: data[3]
           }
         }
             },
-    {"name": "array_literal", "symbols": [{"literal":"{"}, "_", {"literal":"}"}], "postprocess": 
+    {"name": "sequence_literal", "symbols": ["optional_tag", {"literal":"{"}, "_", {"literal":"}"}], "postprocess": 
         (data) => {
+          const tagName = data[0] || 'array';
+           if ( tagName === 'dict'){
+            throw new error('You tagged a sequence as a dict')
+          }
           return {
-            type: 'array_literal',
+            type: tagName + '_literal',
             items: []
           }
         }
             },
+    {"name": "optional_tag", "symbols": [], "postprocess": () => null},
+    {"name": "optional_tag", "symbols": ["tag"], "postprocess": id},
+    {"name": "tag", "symbols": [(lexer.has("less_than") ? {type: "less_than"} : less_than), "tag_name", (lexer.has("greater_than") ? {type: "greater_than"} : greater_than)], "postprocess":  
+        (data) => data[1] 
+            },
+    {"name": "tag_name", "symbols": [{"literal":"array"}], "postprocess": id},
+    {"name": "tag_name", "symbols": [{"literal":"dict"}], "postprocess": id},
+    {"name": "tag_name", "symbols": [{"literal":"set"}], "postprocess": id},
     {"name": "_", "symbols": []},
     {"name": "_", "symbols": ["__"]},
     {"name": "__", "symbols": [(lexer.has("whitespace") ? {type: "whitespace"} : whitespace)]}
